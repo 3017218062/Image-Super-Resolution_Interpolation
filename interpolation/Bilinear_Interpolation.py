d9f20f53848b797c89028457a7016c0677da5654
301218062 <cxdzbsufei@tju.edu.cn>
2019-11-10 19:01:49 +0800

    Bilinear interpolation.

--- interpolation/Bilinear_Interpolation.py
+++ interpolation/Bilinear_Interpolation.py
@@ -7,22 +7,28 @@ def bilinearInterpolation(image, newShape=(1024, 1024)):
     hScale, wScale = oldHeight / newHeight, oldWidth / newWidth
 
     for i in range(newHeight):
-        x = (i + 1) * hScale
-        xUp, xDown = int(x)-1, int(x + 1)-1
+        x = (i + 0.5) * hScale - 0.5
+        xUp, xDown = int(x), int(x + 1)
+        if xDown == oldHeight:
+            xDown -= 1
+        x = x % 1
         for j in range(newWidth):
-            y = (j + 1) * wScale
-            yLeft, yRight = int(y)-1, int(y + 1)-1
+            y = (j + 0.5) * wScale - 0.5
+            yLeft, yRight = int(y), int(y + 1)
+            if yRight == oldWidth:
+                yRight -= 1
+            y = y % 1
 
-            # X,Y=np.asarray([1-x,x]),np.asarray([[1-y],[y]])
-            # for k in range(3):
-            #     adjacentLattice = np.asarray([[image[xUp,yLeft,[k]],image[xUp,yRight,[k]]],[image[xDown,yLeft,[k]],image[xDown,yRight,[k]]]])
-            #     newImage[i,j,[k]]=np.dot(np.dot(X,adjacentLattice),Y)
-            print(x,y,xUp, xDown,yLeft, yRight)
-            break
-        break
-    return newImage
+            X, Y = np.asarray([1 - x, x]), np.asarray([[1 - y], [y]])
+            for k in range(3):
+                adjacentLattice = np.asarray([[image[xUp, yLeft, k], image[xUp, yRight, k]],
+                                              [image[xDown, yLeft, k], image[xDown, yRight, k]]])
+                newImage[i, j, k] = np.dot(np.dot(X, adjacentLattice), Y)
+
+    return newImage.astype(np.uint8)
+
 
 if __name__ == "__main__":
     image = imageLoad()
     newImage = bilinearInterpolation(image, newShape=(1024, 1024))
-    imageSave(newImage)
+    imageSave(newImage, "flower_bilinear.jpg")
